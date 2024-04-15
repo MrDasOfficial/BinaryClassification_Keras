cf2f45422e7d271962f73bfe80e6200190a14c7d
Aswin Das <150241897+MrDasOfficial@users.noreply.github.com>
2024-04-15 13:18:31 +0530

    Update diabetes.py

--- diabetes.py
+++ diabetes.py
@@ -3,7 +3,7 @@ import numpy as np
 from keras.models import Sequential
 from keras.layers import Dense
 
-data = np.loadtxt("pima-indians-diabetes.csv",delimiter=",")
+data = np.loadtxt("pima-indians-diabetes.csv",delimiter=",") # loading dataset, delimiter is used for avoiding ','
 
 x_input = data[:,:-1]
 y_output = data[:,-1]
@@ -19,17 +19,18 @@ model.add(Dense(12,activation = "relu", input_shape=(8,)))
 model.add(Dense(8,activation="relu"))
 model.add(Dense(1,activation="sigmoid"))
 
+# relu rectified linear unit activation ==== > f(x) = max(0,x)
+# f(2) = max(0,2) ==> 2, f(-2) = max(0,-2) ==> 0
+
+# sigmoid ====> s(x) = 0<ans>1.... 1/(1+e^-x)
+#  e = 2.714 Euler's constant
+# s(2) ==>  0.88
+
 model.compile(loss="binary_crossentropy",optimizer="adam",metrics = ["accuracy"])
 model.fit(x_input,y_output,epochs = 100,batch_size=10)
 
 
 
 
-# relu rectified linear unit activation ==== > f(x) = max(0,x)
 
-# sigmoid ====> s(x) = 0<ans>1.... 1/(1+e^-x)
-
-#  e = 2.714 Euler's constant
-
-# s(2) ==>  0.88
 
