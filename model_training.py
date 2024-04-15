47d45e28b74a5379c35cbe809d6fd6bc3719698c
Aswin Das <150241897+MrDasOfficial@users.noreply.github.com>
2024-04-15 15:03:04 +0530

    Update model_training.py

--- model_training.py
+++ model_training.py
@@ -23,28 +23,31 @@ y_test = y_output[760:]
 
 # creating model by adding layers ------------
 
-model = Sequential()
-model.add(Dense(32,activation = "relu", input_shape=(8,)))
-model.add(Dense(128,activation="relu"))
-model.add(Dense(64,activation="relu"))
-model.add(Dense(22,activation="relu"))
-model.add(Dense(1,activation="sigmoid"))
+def model_building():
+  model = Sequential()
+  model.add(Dense(32,activation = "relu", input_shape=(8,)))
+  model.add(Dense(128,activation="relu"))
+  model.add(Dense(64,activation="relu"))
+  model.add(Dense(22,activation="relu"))
+  model.add(Dense(1,activation="sigmoid"))
+  
+  # relu rectified linear unit activation ==== > f(x) = max(0,x)
+  # f(2) = max(0,2) ==> 2, f(-2) = max(0,-2) ==> 0
+  
+  # sigmoid ====> s(x) = 0<ans>1.... 1/(1+e^-x)
+  #  e = 2.714 Euler's constant
+  # s(2) ==>  0.88
+  
+  model.compile(loss="binary_crossentropy",optimizer="adam",metrics = ["accuracy"])
+  model.fit(x_train,y_train,epochs = 500,batch_size=5)
+  
+  model.save("model._diabetics.h5")
+  # the model has a training accuracy of 0.9987 and is saved into the "model_diabetics.h5" file
 
-# relu rectified linear unit activation ==== > f(x) = max(0,x)
-# f(2) = max(0,2) ==> 2, f(-2) = max(0,-2) ==> 0
 
-# sigmoid ====> s(x) = 0<ans>1.... 1/(1+e^-x)
-#  e = 2.714 Euler's constant
-# s(2) ==>  0.88
-
-model.compile(loss="binary_crossentropy",optimizer="adam",metrics = ["accuracy"])
-model.fit(x_train,y_train,epochs = 500,batch_size=5)
-
-model.save("model._diabetics.h5")
-# the model has a training accuracy of 0.9987 and is saved into the "model_diabetics.h5" file
-
-
-
+if __name__ == "__main__":
+  model_building()
+  # to ensure the model is not trained unintentionally if the page is imported to another page
 
 
 
